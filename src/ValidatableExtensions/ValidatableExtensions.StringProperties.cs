506315e6b7ffa65eb10cd9917a17aa04324344f0
DevJonie <jkaddo91@gmail.com>
2022-03-08 01:21:07 +0000

    Implement Equal/NotEqual for StringProperty

--- src/ValidatableExtensions/ValidatableExtensions.StringProperties.cs
+++ src/ValidatableExtensions/ValidatableExtensions.StringProperties.cs
@@ -124,4 +124,34 @@ public static partial class ValidatableExtensions
 
         return ref validatable;
     }
+
+    /// <summary>
+    /// Throws an exception if the length of string returned from the given <paramref name="func"/> is equal to <paramref name="length"/>.
+    /// </summary>
+    /// <remarks>
+    /// The default exception thrown is an <see cref="ArgumentException"/>.
+    /// </remarks>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ref readonly Validatable<TValue> IfLengthEquals<TValue>(this in Validatable<TValue> validatable, Func<TValue, string> func, int length, [CallerArgumentExpression("func")] string? funcName = null)
+        where TValue : notnull
+    {
+        Validator.IfLengthEquals(func(validatable.Value), $"{validatable.ParamName}: {funcName}", validatable.ExceptionCustomizations, length);
+
+        return ref validatable;
+    }
+
+    /// <summary>
+    /// Throws an exception if the length of string returned from the given <paramref name="func"/> is not equal to <paramref name="length"/>.
+    /// </summary>
+    /// <remarks>
+    /// The default exception thrown is an <see cref="ArgumentException"/>.
+    /// </remarks>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ref readonly Validatable<TValue> IfLengthNotEquals<TValue>(this in Validatable<TValue> validatable, Func<TValue, string> func, int length, [CallerArgumentExpression("func")] string? funcName = null)
+        where TValue : notnull
+    {
+        Validator.IfLengthNotEquals(func(validatable.Value), $"{validatable.ParamName}: {funcName}", validatable.ExceptionCustomizations, length);
+
+        return ref validatable;
+    }
 }
