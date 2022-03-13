283e6d6ebfc95c59ba6c01da48e40861b16a3fce
Jeremy <jenkenkur@gmail.com>
2022-03-13 23:48:01 +0100

    Implement Regex IfMatches and IfNotMatches

--- src/ValidatableExtensions/ValidatableExtensions.StringProperties.cs
+++ src/ValidatableExtensions/ValidatableExtensions.StringProperties.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace Throw;
 
 /// <summary>
@@ -306,6 +308,68 @@ public static partial class ValidatableExtensions
         where TValue : notnull
     {
         Validator.ThrowIfNotContains(func(validatable.Value), $"{validatable.ParamName}: {funcName}", validatable.ExceptionCustomizations, otherString, comparisonType);
+
+        return ref validatable;
+    }
+
+    /// <summary>
+    /// Throws an exception if the string returned from the given <paramref name="func"/> matches the given <paramref name="regexPattern"/>.
+    /// Default <paramref name="regexOptions"/> is <see cref="RegexOptions.None"/>.
+    /// </summary>
+    /// <remarks>
+    /// The default exception thrown is an <see cref="ArgumentException"/>.
+    /// </remarks>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ref readonly Validatable<TValue> IfMatches<TValue>(this in Validatable<TValue> validatable, Func<TValue, string> func, string regexPattern, RegexOptions regexOptions = RegexOptions.None, [CallerArgumentExpression("func")] string? funcName = null)
+        where TValue : notnull
+    {
+        Validator.ThrowIfMatches(func(validatable.Value), $"{validatable.ParamName}: {funcName}", validatable.ExceptionCustomizations, regexPattern, regexOptions);
+
+        return ref validatable;
+    }
+
+    /// <summary>
+    /// Throws an exception if the string returned from the given <paramref name="func"/> matches the given <paramref name="regex"/>.
+    /// </summary>
+    /// <remarks>
+    /// The default exception thrown is an <see cref="ArgumentException"/>.
+    /// </remarks>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ref readonly Validatable<TValue> IfMatches<TValue>(this in Validatable<TValue> validatable, Func<TValue, string> func, Regex regex, [CallerArgumentExpression("func")] string? funcName = null)
+        where TValue : notnull
+    {
+        Validator.ThrowIfMatches(func(validatable.Value), $"{validatable.ParamName}: {funcName}", validatable.ExceptionCustomizations, regex);
+
+        return ref validatable;
+    }
+
+    /// <summary>
+    /// Throws an exception if the string returned from the given <paramref name="func"/> does not match the given <paramref name="regexPattern"/>.
+    /// Default <paramref name="regexOptions"/> is <see cref="RegexOptions.None"/>.
+    /// </summary>
+    /// <remarks>
+    /// The default exception thrown is an <see cref="ArgumentException"/>.
+    /// </remarks>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ref readonly Validatable<TValue> IfNotMatches<TValue>(this in Validatable<TValue> validatable, Func<TValue, string> func, string regexPattern, RegexOptions regexOptions = RegexOptions.None, [CallerArgumentExpression("func")] string? funcName = null)
+        where TValue : notnull
+    {
+        Validator.ThrowIfNotMatches(func(validatable.Value), $"{validatable.ParamName}: {funcName}", validatable.ExceptionCustomizations, regexPattern, regexOptions);
+
+        return ref validatable;
+    }
+
+    /// <summary>
+    /// Throws an exception if the string returned from the given <paramref name="func"/> does not match the given <paramref name="regex"/>.
+    /// </summary>
+    /// <remarks>
+    /// The default exception thrown is an <see cref="ArgumentException"/>.
+    /// </remarks>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ref readonly Validatable<TValue> IfNotMatches<TValue>(this in Validatable<TValue> validatable, Func<TValue, string> func, Regex regex, [CallerArgumentExpression("func")] string? funcName = null)
+        where TValue : notnull
+    {
+        Validator.ThrowIfNotMatches(func(validatable.Value), $"{validatable.ParamName}: {funcName}", validatable.ExceptionCustomizations, regex);
 
         return ref validatable;
     }
